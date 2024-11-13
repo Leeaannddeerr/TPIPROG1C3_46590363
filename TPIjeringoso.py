1d882e171009b6737eeefbcd1e920031827efbe5
vickott <vicki02peretti@gmail.com>
2024-11-13 19:05:53 -0300

    agregue emojis

--- TPIjeringoso.py
+++ TPIjeringoso.py
@@ -41,21 +41,21 @@ def jeringoso(palabra):#recibe palabra y convierte en jeringoso
 def main():#menu
      diccionario = {} #diccionario vacio para almacenar las palabras y las versiones de jeringoso
      while True:#pide la palabra, verifica si es salir, y si no lo es lo convierte a jeringoso la palabra
-          print("\nMenu de opciones:")
-          print("1. Ingresar una palabra") 
-          print("2. Mostrar historial de palabras en jeringoso")
-          print("3. Salir") 
-          opcion = input("Elige una de las siguientes opciones (1, 2, o 3):").strip()
+          print("\nMenu de opciones:📖")
+          print("1. Ingresar una palabra ✍️") 
+          print("2. Mostrar historial de palabras en jeringoso 📚")
+          print("3. Salir 📤") 
+          opcion = input("Elige una de las siguientes opciones (1, 2, o 3): ").strip()
           if opcion == "1":  # Opción para ingresar una palabra
             palabra = input("Ingresa una palabra: ").strip()
             palabra_jeringosa = jeringoso(palabra)
             diccionario[palabra] = palabra_jeringosa  # Guardar la palabra y su versión en jeringoso
-            print(f"Palabra en jeringoso: {palabra_jeringosa}")
+            print("Palabra en jeringoso:",palabra_jeringosa)
           elif opcion == "2": 
             if diccionario: 
                 print("\nHistorial de palabras en jeringoso: ")
                 for palabra, palabra_jeringosa in diccionario.items():
-                  print(palabra,"-->",palabra_jeringosa)
+                  print(palabra," ➡️➡️➡️ ",palabra_jeringosa)
             else:
                 print("No se han ingresado palabras") 
           elif opcion == "3":
