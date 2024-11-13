f1f291c2b67041bf956e037e5a962bc982fcfaa1
vickott <vicki02peretti@gmail.com>
2024-11-13 19:36:19 -0300

    agregue aclaraciones y otros detalles

--- TPIjeringoso.py
+++ TPIjeringoso.py
@@ -2,7 +2,7 @@ import re
 #funcion para separar la palabra en silabas 
 def separar_silabas(palabra):
     silabas = []
-    palabra = palabra.lower()
+    palabra = palabra.lower() #convierte la palabra a minusculas
     vocales = "aeiou"
     silaba = ""
    
@@ -18,13 +18,12 @@ def separar_silabas(palabra):
                 silabas.append(silaba) #agrega la ultima silaba
     if silaba:
         silabas.append(silaba)
-    return silabas
+    return silabas #devuelve la lista de silabas
 
 def jeringoso(palabra):#recibe palabra y convierte en jeringoso
     jeringosa = '' #cadena que almacena
     vocales = ('a', 'e', 'i', 'o', 'u')#tupla de vocales
-    #silabas = separar_silabas(palabra) #separar la palabra en silabas
-
+   
     silaba_anterior = '' #variable para almacenar la silaba anterior
 
     for i in range(len(palabra)):
@@ -32,20 +31,20 @@ def jeringoso(palabra):#recibe palabra y convierte en jeringoso
     
 
         if letra.lower()in vocales:
-            jeringosa +=  letra + 'p' + letra 
+            jeringosa +=  letra + 'p' + letra #se agrega la letra, seguida de p y despues de nuevo por la letra
         else:
-            jeringosa += letra
+            jeringosa += letra #si no es vocal, solo se agrega a la cadena
                  
-    return jeringosa
+    return jeringosa #devuelve la palabra convertida a jeringoso
 
 def main():#menu
      diccionario = {} #diccionario vacio para almacenar las palabras y las versiones de jeringoso
      while True:#pide la palabra, verifica si es salir, y si no lo es lo convierte a jeringoso la palabra
           print("\nMenu de opciones:📖")
-          print("1. Ingresar una palabra ✍️") 
+          print("1. Ingresar una palabra ✍🏼") 
           print("2. Mostrar historial de palabras en jeringoso 📚")
           print("3. Salir 📤") 
-          opcion = input("Elige una de las siguientes opciones (1, 2, o 3): ").strip()
+          opcion = input("Elige una de las siguientes opciones (1, 2, o 3): ").strip() #pide al usuario una opcion y elimina los espacios en blanco al inicio y final
           if opcion == "1":  # Opción para ingresar una palabra
             palabra = input("Ingresa una palabra: ").strip()
             palabra_jeringosa = jeringoso(palabra)
@@ -59,7 +58,7 @@ def main():#menu
             else:
                 print("No se han ingresado palabras") 
           elif opcion == "3":
-              print("Gracias! Hasta la próxima")
+              print("Gracias! Hasta la próxima👋🏼")
               break
           else:
               print("Opción no válida. Por favor ingrese una opción válida (1, 2, o 3)")
