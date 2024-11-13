f0167396575f84a28790217d26ef58ebfb0d5555
vickott <vicki02peretti@gmail.com>
2024-11-13 13:12:27 -0300

    agregue los #

--- TPIjeringoso.py
+++ TPIjeringoso.py
@@ -1,7 +1,7 @@
-def jeringoso(palabra):
+def jeringoso(palabra):#recibe palabra y convierte en jeringoso
     jeringosa = ''
-    vocales = ('a', 'e', 'i', 'o', 'u')
-    for i in range(len(palabra)):
+    vocales = ('a', 'e', 'i', 'o', 'u')#tupla de vocales
+    for i in range(len(palabra)):#for para recorrer, si la letra es vocal agrega p seguido de la vocal
         letra=palabra[i]
         if letra.lower() in vocales:
             jeringosa+="p"+letra
@@ -10,13 +10,14 @@ def jeringoso(palabra):
     return jeringosa
 
 
-def main():
-     diccionario= {}
-     while True:
+def main():#maneja la interaccion con el usuario
+     diccionario= {} #diccionario vacio para almacenar las palabras y las versiones de jeringoso
+     while True:#pide la palabra, verifica si es sair, y si no lo es lo convierte a jeringoso la palabra
           palabra=input("Ingresa una palabra o escribe salir para terminar: ")
-          if palabra.lower()== salir:
+          if palabra.lower()== "salir":
                break
           palabra_jeringosa=jeringoso(palabra)
           diccionario[palabra]=palabra_jeringosa
           print("Palabra en jeringoso:",palabra_jeringosa)
 print("Diccionario de palabras en jeringoso:")
+
