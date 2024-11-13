4460f3427c0cb3fb22a021ccafbc72ea37b599c1
Gastón Godoy Dechecco <gaston.vm2000@hotmail.com>
2024-11-13 19:57:47 -0300

    .

--- Menu.py
+++ Menu.py
@@ -1,6 +1,6 @@
 import TPIjeringoso
 import TPI_escalones_Gastón_Godoy_Dechecco as escalones
-
+import TPI_tres_en_raya
 
 def menu():
     print("Bienvenido a los 5 juegos")
@@ -21,13 +21,13 @@ def menu():
         elif usuario == "1":
             pass
         elif usuario == "2":
-            TPIjeringoso
+            TPIjeringoso.main()
         elif usuario == "3":
-            escalones
+            escalones.general()
         elif usuario == "4":
             pass
         elif usuario == "5":
-            pass
+            TPI_tres_en_raya.inicio_j()
 
 if __name__ == "__main__":
     menu()
